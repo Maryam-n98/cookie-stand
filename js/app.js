34141400a3349697487bc88109f7358ea075a623
maryam alnajjar <mariam.mohannad98@gmail.com>
2021-04-06 10:35:19 +0300

    Finall edited in class07

--- js/app.js
+++ js/app.js
@@ -334,28 +334,34 @@ let finalRow =function () {
         let am6col = document.createElement('th')
         am6.appendChild(am6col);
         am6col.textContent = `total`;
-        for (let i = 0; i <= hours.length; i++) {
+        for (let i = 0; i < hours.length; i++) {
             // let pm7 = document.createElement('tr');
             // table.appendChild(pm7);
             
             let totalPerHour = 0;
+            let totalTotal = 0;
             for (let j = 0; j < shopArr.length; j++) {
-                totalPerHour += shopArr[j].cookieNum[i]
+                totalPerHour += shopArr[j].cookieNum[i] ;
+                totalTotal += shopArr[j].cookieNum[i] ;
             }
             let footerData = document.createElement('th');
             am6.appendChild(footerData);
             footerData.textContent=totalPerHour;
         }
+        let totalTh = document.createElement('th');
+        am6.appendChild(totalTh);
+        totalTh.textContent=totalTotal;
         
-        let totalTotal = [];
-        for (let b = 0; b < 5 ; b++) {
+        
+        // let totalTotal = [];
+        // for (let b = 0; b < 5 ; b++) {
             
-          totalTotal += totalTotal[b]. this.totalCookie;
-        }
+        //   totalTotal += totalTotal[b]. this.totalCookie;
+        // }
        
       
     
         }
-    
+    totalTh();
     finalRow();
     
